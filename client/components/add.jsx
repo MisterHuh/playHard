f4cbbf485fdc4c87ce923df804fd847726bab1fb
MisterHuh <jaehuh86@yahoo.com>
2019-12-16 00:38:13 -0800

    ready for endpoint

--- client/components/add.jsx
+++ client/components/add.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import Moment from 'react-moment';
-import 'moment-timezone';
+// import Moment from 'react-moment';
+// import 'moment-timezone';
 
 import Dropdown from "react-dropdown";
 import 'react-dropdown/style.css'
@@ -19,7 +19,7 @@ export default class Add extends React.Component {
       date: new Date(),
       category: "Spendings",
       subCategory: "Food",
-      cc: "AmEx",
+      cc: "Sapphire",
       amount: "",
       description: "",
       notes: ""
@@ -30,41 +30,44 @@ export default class Add extends React.Component {
     this.ccHandleChange = this.ccHandleChange.bind(this);
     this.inputHandleChange = this.inputHandleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.formatDate = this.formatDate.bind(this);
+    this.addEntry = this.addEntry.bind(this);
   }
 
-  dateHandleChange(date) {
+  // componentDidMount() {
+  //   this.dateHandleChange();
+  // }
 
+  dateHandleChange(date) {
+    // console.log("date is: ", date);
     // let current_datetime = date;
     // let formatted_date = (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + "-" + current_datetime.getFullYear()
     // console.log("formatted_date is: ", formatted_date)
-
     // this.setState({ date: formatted_date });
     this.setState({ date });
   };
 
   categoryHandleChange(e) {
-    console.log("event is: ", e);
-    console.log("event.value is: ", e.value);
+    // console.log("event is: ", e);
+    // console.log("event.value is: ", e.value);
     this.setState({ category: e.value })
   }
 
   subCategoryHandleChange(e) {
-    console.log("event is: ", e);
-    console.log("event.value is: ", e.value);
+    // console.log("event is: ", e);
+    // console.log("event.value is: ", e.value);
     this.setState({ subCategory: e.value})
   }
 
   ccHandleChange(e) {
-    console.log("event is: ", e);
-    console.log("event.value is: ", e.value);
+    // console.log("event is: ", e);
+    // console.log("event.value is: ", e.value);
     this.setState({ cc: e.value })
   }
 
   inputHandleChange(e) {
     e.preventDefault();
     const { name, value } = e.target;
-    // console.log("e.target.name is: ", e.target.name);
-    // console.log("e.target.value is: ", e.target.value);
     switch(name) {
       case "amount":
         this.setState({ amount: e.target.value});
@@ -79,7 +82,54 @@ export default class Add extends React.Component {
   }
 
   handleSubmit() {
-    console.log("handleSubmit clicked");
+    const formatted_date = this.formatDate();
+    // console.log("formatted_date is: ", formatted_date);
+
+    this.addEntry(formatted_date)
+  }
+
+  formatDate() {
+    let current_datetime = this.state.date;
+    let formatted_date = (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + "-" + current_datetime.getFullYear()
+    return formatted_date;
+  }
+
+  addEntry(formatted_date) {
+
+    const entry = {
+      date: formatted_date,
+      category: this.state.category,
+      subCategory: this.state.subCategory,
+      cc: this.state.cc,
+      amount: this.state.amount,
+      description: this.state.amount,
+      notes: this.state.notes
+    }
+
+    const req = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(entry)
+    };
+
+    console.log("final entry is: ", entry);
+
+    // fetch(`/api/cart.php`, req)
+    //   .then(response => response.json())
+    //   .catch(error => {
+    //     console.error('delete error: ', error);
+    //   });
+    // this.retrieveCart();
+
+    this.setState({
+      date: new Date(),
+      category: "Spendings",
+      subCategory: "Food",
+      cc: "Sapphire",
+      amount: "",
+      description: "",
+      notes: ""
+     })
   }
 
   render() {
@@ -164,6 +214,7 @@ export default class Add extends React.Component {
 
         <div className="mx-4">
           <input
+            value={this.state.amount}
             onChange={this.inputHandleChange}
             placeholder="$ 00.00"
             name="amount"
@@ -173,15 +224,17 @@ export default class Add extends React.Component {
 
         <div className="mx-4">
           <input
+            value = {this.state.description}
             onChange={this.inputHandleChange}
-            placeholder="Descriptions"
-            name="descriptions"
+            placeholder="Description"
+            name="description"
             className="amount mt-4"
           />
         </div>
 
         <div className="mx-4">
           <input
+            value = {this.state.notes}
             onChange={this.inputHandleChange}
             placeholder="Notes"
             name="notes"
