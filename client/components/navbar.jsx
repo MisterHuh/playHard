eeae928f89d477603a7d932a4326f6ae1317d7cf
MisterHuh <jaehuh86@yahoo.com>
2019-12-16 23:17:12 -0800

    fixed conditional background color for navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -4,59 +4,39 @@ import { generateKeyPair } from "crypto";
 export const Navbar = props => {
 
   let currentView = props.currentView;
-  let addBackfill, currentBackfill, historyBackfill;
-
-  if (currentView = "add") {
-    addBackfill = {
-      backgroundColor: "#96D1E3",
+  let add, current, history;
+  if (currentView === "add") {
+    add = {
+      background: "#999",
       color: "white"
-    }
-  } else if (currentView = "current") {
-    currentBackfill = {
-      backgroundColor: "#96D1E3",
+    };
+  } else if (currentView === "current") {
+    current = {
+      background: "#999",
+      color: "white"
+    };
+  } else if (currentView === "history") {
+    history = {
+      background: "#999",
       color: "white"
     }
   }
-
-  // if (currentView = "add") {
-  //   addBackfill = {
-  //     backgroundColor: "#96D1E3",
-  //     color: "white"
-  //   };
-  //   currentBackfill = {
-  //     backgroundColor: "white"
-  //   };
-  //   historyBackfill = {
-  //     backgroundColor: "white"
-  //   };
-  // } else if (currentView = "current") {
-  //   addBackfill = {
-  //     backgroundColor: "white",
-  //   };
-  //   currentBackfill = {
-  //     backgroundColor: "#96D1E3",
-  //     color: "white"
-  //   };
-  //   historyBackfill = {
-  //     backgroundColor: "white"
-  //   };
-  // }
 
   let setView = props.setView;
 
   return (
       <div className="d-flex flex-row justify-content-center">
-      <div className="navbarButtonContainer border" style={addBackfill}>
+      <div className="navbarButtonContainer border" style={add}>
         <i
           onClick={() => setView("add")}
           className="fas fa-plus"></i>
       </div>
-      <div className="navbarButtonContainer border" style={currentBackfill}>
+      <div className="navbarButtonContainer border" style={current}>
         <i
           onClick={() => setView("current")}
           className="fas fa-check"></i>
       </div>
-      <div className="navbarButtonContainer border" style={historyBackfill}>
+      <div className="navbarButtonContainer border" style={history}>
         <i
           onClick={() => setView("history")}
           className="fas fa-history"></i>
