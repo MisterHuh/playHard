2b1149c6de3acb54f90037ed2556fbed35379816
MisterHuh <jaehuh86@yahoo.com>
2019-12-19 21:54:53 -0800

    initial flexbox setup

--- client/components/current.jsx
+++ client/components/current.jsx
@@ -6,11 +6,20 @@ export default class Current extends React.Component {
   }
   render() {
     return (
-      <div className="currentFixed my-4">
-        <div className="item item-1">item-1</div>
-        <div className="item item-2">item-2</div>
-        <div className="item item-3">item-3</div>
+
+      <div className="currentWrapper">
+        <div className="currentContainer">Container 1</div>
+        <div className="currentContainer">Container 2</div>
+        <div className="currentContainer">Container 3</div>
       </div>
+
+
+      // <div className="currentFixed my-4">
+      //   <div className="item item-1">item-1</div>
+      //   <div className="item item-2">item-2</div>
+      //   <div className="item item-3">item-3</div>
+      // </div>
+
     )
   }
 }
