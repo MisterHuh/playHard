b0c6c6db3f3e092a72cc930587f585e9726d0089
MisterHuh <jaehuh86@yahoo.com>
2019-12-20 23:26:35 -0800

    working on desing

--- client/components/history.jsx
+++ client/components/history.jsx
@@ -7,7 +7,48 @@ export default class History extends React.Component {
 
   render() {
     return (
-      <div>history</div>
+
+      <React.Fragment>
+
+      <div className="currentWrapper mt-4">
+
+        <div className="currentContainer">
+          <div className="currentBox"></div>
+          <div className="currentBox">Total Spent</div>
+          <div className="currentBox">Credit</div>
+          <div className="currentBox">Remaining</div>
+        </div>
+
+        <div className="currentContainer">
+          <div className="currentBox">Spendings</div>
+          <div className="currentBox">$12.00</div>
+          <div className="currentBox">$12.00</div>
+          <div className="currentBox">$12.00</div>
+        </div>
+
+        <div className="currentContainer">
+          <div className="currentBox">Fixed</div>
+          <div className="currentBox">$12.00</div>
+          <div className="currentBox">$12.00</div>
+          <div className="currentBox">$12.00</div>
+        </div>
+
+      </div>
+
+      <div className="mt-4 currentTableHeaderContainer">
+        <div className="currentTableHeader">
+          <div className="currentTableRow">Date</div>
+          <div className="currentTableRow">subCategory</div>
+          <div className="currentTableRow">cc</div>
+          <div className="currentTableRow">Amount</div>
+          <div className="currentTableRow">Store</div>
+          <div className="currentTableRow">Notes</div>
+        </div>
+        {/* <CurrentData current={this.state.current} /> */}
+      </div>
+
+      </React.Fragment>
+
     )
   }
 }
