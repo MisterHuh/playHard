f06ed781dd2dabd0cebd519a5328ef886dbc30b3
MisterHuh <jaehuh86@yahoo.com>
2019-12-24 12:06:18 -0800

    attempting to add Intl.NumberFormat

--- client/components/currentData.jsx
+++ client/components/currentData.jsx
@@ -17,7 +17,7 @@ export const CurrentData = props => {
               <div className="currentDataBox">{entry.date}</div>
               <div className="currentDataBox">{entry.subCategory}</div>
               <div className="currentDataBox">{entry.cc}</div>
-              <div className="currentDataBox">{entry.amount}</div>
+              <div className="currentDataBox">{"$" + entry.amount.toFixed(2)}</div>
               <div className="currentDataBox">{entry.store}</div>
               <div className="currentDataBox">{entry.notes}</div>
 
