750ec8cedf353b30d55e05e128f05856eeb45d82
MisterHuh <jaehuh86@yahoo.com>
2020-01-04 16:36:01 -0800

    all data is being retrieved. NOT the most conventional way

--- server/public/api/get.php
+++ server/public/api/get.php
@@ -5,27 +5,75 @@ require_once("db_connection.php");
 set_exception_handler("error_handler");
 startUp();
 
-$sunday = date('Y-m-d', strtotime('sunday this week'));
-print($sunday);
+// $sunday = date('Y-m-d', strtotime('sunday this week'));
+// print("sunday is: " . $sunday);
 
-$query =  "SELECT * FROM spendings ORDER BY date ASC";
+$spndingQuery =  "SELECT * FROM spendings ORDER BY date ASC";
 
-$result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $spndingQuery);
 
 if (!$result) {
   throw new Exception('error with query: ' . mysqli_error($conn));
 };
 
-$output = [];
+$currentData = [];
 while ($row = mysqli_fetch_assoc($result)) {
-  $output[] = $row;
+  $currentData[] = $row;
 };
 
-if ($output === []) {
+// if ($spendingsOutput === []) {
+//   print("[]");
+//   exit();
+// } else {
+//   print(json_encode($spendingsOutput));
+// }
+
+$creditsQuery =  "SELECT * FROM credits ORDER BY date ASC";
+
+$result = mysqli_query($conn, $creditsQuery);
+
+if (!$result) {
+  throw new Exception('error with query: ' . mysqli_error($conn));
+};
+
+// $currentData = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $currentData[] = $row;
+};
+
+// if ($creditsOutput === []) {
+//   print("[]");
+//   exit();
+// } else {
+//   print(json_encode($creditsOutput));
+// }
+
+$fixedQuery =  "SELECT * FROM fixed ORDER BY date ASC";
+
+$result = mysqli_query($conn, $fixedQuery);
+
+if (!$result) {
+  throw new Exception('error with query: ' . mysqli_error($conn));
+};
+
+// $currentData = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $currentData[] = $row;
+};
+
+// if ($fixedOutput === []) {
+//   print("[]");
+//   exit();
+// } else {
+//   print(json_encode($fixedOutput));
+// }
+
+// $currentDataOutput = [];
+if ($currentData === []) {
   print("[]");
   exit();
 } else {
-  print(json_encode($output));
+  print(json_encode($currentData));
 }
 
 
