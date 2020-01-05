cd3ca8121c3e4d25d188046062ac7dac378a5dca
MisterHuh <jaehuh86@yahoo.com>
2020-01-05 14:05:06 -0800

    order by current week now

--- server/public/api/get.php
+++ server/public/api/get.php
@@ -8,7 +8,9 @@ startUp();
 // $sunday = date('Y-m-d', strtotime('sunday this week'));
 // print("sunday is: " . $sunday);
 
-$spndingQuery =  "SELECT * FROM spendings ORDER BY date ASC";
+$spndingQuery =  "SELECT * FROM spendings
+                    WHERE YEARWEEK(date)=YEARWEEK(NOW())
+                    ORDER BY date ASC";
 
 $result = mysqli_query($conn, $spndingQuery);
 
@@ -21,14 +23,9 @@ while ($row = mysqli_fetch_assoc($result)) {
   $currentData[] = $row;
 };
 
-// if ($spendingsOutput === []) {
-//   print("[]");
-//   exit();
-// } else {
-//   print(json_encode($spendingsOutput));
-// }
-
-$creditsQuery =  "SELECT * FROM credits ORDER BY date ASC";
+$creditsQuery =  "SELECT * FROM credits
+                    WHERE YEARWEEK(date)=YEARWEEK(NOW())
+                    ORDER BY date ASC";
 
 $result = mysqli_query($conn, $creditsQuery);
 
@@ -36,19 +33,13 @@ if (!$result) {
   throw new Exception('error with query: ' . mysqli_error($conn));
 };
 
-// $currentData = [];
 while ($row = mysqli_fetch_assoc($result)) {
   $currentData[] = $row;
 };
 
-// if ($creditsOutput === []) {
-//   print("[]");
-//   exit();
-// } else {
-//   print(json_encode($creditsOutput));
-// }
-
-$fixedQuery =  "SELECT * FROM fixed ORDER BY date ASC";
+$fixedQuery =  "SELECT * FROM fixed
+                    WHERE YEARWEEK(date)=YEARWEEK(NOW())
+                    ORDER BY date ASC";
 
 $result = mysqli_query($conn, $fixedQuery);
 
@@ -56,19 +47,10 @@ if (!$result) {
   throw new Exception('error with query: ' . mysqli_error($conn));
 };
 
-// $currentData = [];
 while ($row = mysqli_fetch_assoc($result)) {
   $currentData[] = $row;
 };
 
-// if ($fixedOutput === []) {
-//   print("[]");
-//   exit();
-// } else {
-//   print(json_encode($fixedOutput));
-// }
-
-// $currentDataOutput = [];
 if ($currentData === []) {
   print("[]");
   exit();
