bc382f7c2ad3fa18b34b9ec46e80fb184384f6d5
MisterHuh <jaehuh86@yahoo.com>
2020-01-18 23:47:48 -0800

    created dummy view for the actual view. Will need to re-strcutrue the css

--- client/components/history.jsx
+++ client/components/history.jsx
@@ -117,24 +117,18 @@ export default class History extends React.Component {
         {/* one */}
         <div className="currentSummaryContainer">
           <div className="currentSummary">
-            <div className="budget tooltipParent">
-              Total Budget
-              <span className="tooltipText">{"Current week is: " + this.state.currentWeekNumber}</span>
-            </div>
-            <div className="spendings">Total Spendings</div>
-            <div className="credits">Total Credits</div>
-            <div className="fixed">Total Fixed</div>
-            <div className="remaining">Total Remaining</div>
+            <div className="budget">View</div>
+            <div className="">Start Date</div>
+            <div className="">End Date</div>
+            {/* <div className="">Total Fixed</div> */}
+            {/* <div className="">Total Remaining</div> */}
           </div>
           <div className="currentSummary">
-            <div className="budget tooltipParent">
-              {CurrencyFormatter.format(this.state.totalBudget)}
-              {/* <span className="tooltipText">{"Current week is: " + this.state.currentWeekNumber}</span> */}
-            </div>
-            <div className="spendings">{CurrencyFormatter.format(this.state.totalSpendings)}</div>
-            <div className="credits creditsFontColor">{CurrencyFormatter.format(this.state.totalCredits)}</div>
-            <div className="fixed">{CurrencyFormatter.format(this.state.totalFixed)}</div>
-            <div className="remaining">{CurrencyFormatter.format(this.state.totalRemaining)}</div>
+            <div className="">All</div>
+            <div className="">01/05/20</div>
+            <div className="">01/11/20</div>
+            {/* <div className="">{CurrencyFormatter.format(this.state.totalFixed)}</div> */}
+            {/* <div className="">{CurrencyFormatter.format(this.state.totalRemaining)}</div> */}
           </div>
         </div>
 
