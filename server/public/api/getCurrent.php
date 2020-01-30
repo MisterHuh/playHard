e371e041a7b40068ff86f42008b35454eb0b8308
MisterHuh <jaehuh86@yahoo.com>
2020-01-29 21:31:44 -0800

    accidently deleted geCurrent.php. Readded the file

---
+++ server/public/api/getCurrent.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("functions.php");
+require_once("db_connection.php");
+set_exception_handler("error_handler");
+startUp();
+
+$query = "SELECT * FROM `2020`
+            WHERE YEARWEEK(date)=YEARWEEK(NOW())
+            ORDER BY date ASC";
+
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+  throw new Exception('error with query: ' . mysqli_error($conn));
+};
+
+$currentData = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $currentData[] = $row;
+};
+
+if ($currentData === []) {
+  print("[]");
+  exit();
+} else {
+  print(json_encode($currentData));
+};
