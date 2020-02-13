99d7a3aef4679ab681199f4aa1b9d1df58b52cdc
MisterHuh <jaehuh86@yahoo.com>
2020-02-12 22:47:26 -0800

    making progress

--- client/components/renderSideBox.jsx
+++ client/components/renderSideBox.jsx
@@ -3,28 +3,35 @@ import { CurrencyFormatter } from "./currencyFormatter";
 
 export const RenderSideBox = props => {
 
-  return(
+  if (props.currentIndex) {
+    return (
 
-    <div>
-      { props.current.map(entry => {
+      <div>
+        {props.current.map(entry => {
 
-        let creditsFontColor = entry.amount < 0 ? "creditsFontColor" : null;
+          let creditsFontColor = entry.amount < 0 ? "creditsFontColor" : null;
 
-        return(
+          return (
 
-          <div key={entry.id} className={"currentData"}>
-            <div className="prevRec mt-3">{entry[props.currentIndex]["date"]}</div>
-            <div className="prevRec mt-3">{entry[props.currentIndex]["category"]}</div>
-            <div className="prevRec mt-3">{entry[props.currentIndex]["subcategory"]}</div>
-            <div className="prevRec mt-3">{entry[props.currentIndex]["cc"]}</div>
-            <div className="prevRec mt-3">{CurrencyFormatter.format(entry[props.currentIndex]["amount"])}</div>
-            <div className="prevRec mt-3">{entry[props.currentIndex]["store"]}</div>
-            <div className="prevRec mt-3">{entry[props.currentIndex]["notes"]}</div>
-          </div>
-        )
-      })
+            <div key={entry.id} className={"currentData"}>
+              <div className="prevRec mt-3">{entry[props.currentIndex]["date"]}</div>
+              <div className="prevRec mt-3">{entry[props.currentIndex]["category"]}</div>
+              <div className="prevRec mt-3">{entry[props.currentIndex]["subcategory"]}</div>
+              <div className="prevRec mt-3">{entry[props.currentIndex]["cc"]}</div>
+              <div className="prevRec mt-3">{CurrencyFormatter.format(entry[props.currentIndex]["amount"])}</div>
+              <div className="prevRec mt-3">{entry[props.currentIndex]["store"]}</div>
+              <div className="prevRec mt-3">{entry[props.currentIndex]["notes"]}</div>
+            </div>
+          )
+        })
 
-      }
-    </div>
-  )
+        }
+      </div>
+    )
+  } else {
+    return (
+      <div>TEST</div>
+    )
+  }
+
 }
