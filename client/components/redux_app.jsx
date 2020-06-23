c78f1edd22da0e4234b14e756626c2c490767d0c
MisterHuh <jaehuh86@yahoo.com>
2020-06-22 21:50:45 -0700

    redux basic attempt successful

--- client/components/redux_app.jsx
+++ client/components/redux_app.jsx
@@ -8,6 +8,47 @@ import React from 'react';
 //   )
 // }
 
+
+// STORE
+import { createStore } from 'redux';
+
+
+// ACTION
+const increment = () => {
+  return {
+    type: "INCREMENT" // this can also be called name
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT" // this can also be called name
+  }
+}
+
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counter);
+
+// display it on the console
+// nothing is going to happen YET because it needs to be dispatched
+store.subscribe(() => console.log(store.getState()));
+
+
+// DISPATCH
+store.dispatch(increment());
+
 export const ReduxApp = () => {
   return (
     <div>
