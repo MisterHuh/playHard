25d369ffe9c0effa3e446cc868c46bf3d2de6eea
MisterHuh <jaehuh86@yahoo.com>
2020-08-10 10:42:06 -0700

    basic form structure fin. no errors

--- client/components/redux_reactFinalForm.jsx
+++ client/components/redux_reactFinalForm.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const showResults = async values => {
+  await sleep(500) // simulate server latency
+  window.alert(JSON.stringify(values, undefined, 2))
+};
 
 const ReactFinalForm = () => (
   <div>
     <h1>React Final Form</h1>
-    <h2>Hello, React Alicante!</h2>
-    <h3>final-form.org @ finalformjs</h3>
+
+    <Form onSubmit={showResults}>
+      {({ handleSubmit }) => <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name</label>
+          <Field
+            name='firstName'
+            component='input'
+            placeholder='Fist Name' />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>}
+    </Form>
+
   </div>
 )
 
