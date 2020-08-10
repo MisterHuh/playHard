c8f892a19d480f585510a3be6dbae79c91b364ef
MisterHuh <jaehuh86@yahoo.com>
2020-08-10 09:52:48 -0700

    got reduxForms to work, but will be switching to react-final-form

--- client/components/redux_reduxForm.jsx
+++ client/components/redux_reduxForm.jsx
@@ -1,5 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+
+/* errorMessage prop doesn't exist in regular ,input. */
+/* the youtube tutorial had import Input from './Input', which has built-in errorMessage props*/
+
+// const renderInput = ({input, meta}) => (
+//   <input {...input} type='text' errorMessage={meta.error} />
+// )
 
 const renderInput = (props) => (
   <input {...props.input} type='text' />
@@ -9,17 +16,25 @@ const onSubmit = values => {
   alert(JSON.stringify(values));
 }
 
-const ReduxForm = ({handleSubmit}) => (
+const required = v => {
+  if (!v || v === '') {
+    return 'This field is required';
+  }
+
+  return undefined;
+}
+
+const ReduxForm = ({handleSubmit, valid}) => (
   <div>
     <h2>Redux Form</h2>
     <form onSubmit={handleSubmit}>
-      <Field name='customer-id' component={renderInput}/>
-      <button type='submit'>Submit</button>
+      <Field name='customer-id' component={renderInput} validate={required}/>
+      <button disabled={!valid} type='submit'>Submit</button>
     </form>
   </div>
 )
 
 export default reduxForm({
-  form: 'my-customer-registration-form',
+  form: 'add_entry',
   onSubmit
 })(ReduxForm)
