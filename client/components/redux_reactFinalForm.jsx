a8da7dff145e763bc4c6d947e869197922d9e429
MisterHuh <jaehuh86@yahoo.com>
2020-08-10 10:16:50 -0700

    start of youtube follow along for React-Final-Form

---
+++ client/components/redux_reactFinalForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReactFinalForm = () => (
+  <div>
+    <h1>React Final Form</h1>
+    <h2>Hello, React Alicante!</h2>
+    <h3>final-form.org @ finalformjs</h3>
+  </div>
+)
+
+export default ReactFinalForm;
--- client/containers/redux_add.jsx
+++ client/containers/redux_add.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import SummaryBox from '../components/redux_summaryBox';
 import SideBox from '../components/redux_sideBox';
 import Form from '../components/redux_form';
+import ReactFinalForm from '../components/redux_reactFinalForm'
 // import ReduxForm from '../components/redux_reduxForm';
 
 // class Add extends Component {}
 
 const Add = () => (
     <React.Fragment>
-        <Form />,
+        {/* <Form />,
         <SummaryBox />,
-        <SideBox />
+        <SideBox /> */}
+        <ReactFinalForm />
         {/* <ReduxForm /> */}
     </React.Fragment>
 )
