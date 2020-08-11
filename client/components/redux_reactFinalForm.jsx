e396ecc0ff0ef96dbf066aa7abaf7e49bf0173a6
MisterHuh <jaehuh86@yahoo.com>
2020-08-11 09:46:06 -0700

    adding form validations & error messages to reactFinalForm

--- client/components/redux_reactFinalForm.jsx
+++ client/components/redux_reactFinalForm.jsx
@@ -1,52 +1,57 @@
-import React from 'react';
-import { Form, Field } from 'react-final-form';
+import React from "react";
+import { Form, Field } from "react-final-form";
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-const showResults = async values => {
-  await sleep(500) // simulate server latency
-  window.alert(JSON.stringify(values, undefined, 2))
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const showResults = async (values) => {
+  await sleep(500); // simulate server latency
+  window.alert(JSON.stringify(values, undefined, 2));
 };
+
+const required = (value) => (value ? undefined : "Required");
 
 const ReactFinalForm = () => (
   <div>
     <h1>React Final Form</h1>
 
     <Form onSubmit={showResults}>
-      {({ handleSubmit, submitting, values }) => <form onSubmit={handleSubmit}>
+      {({ handleSubmit, submitting, values }) => (
+        <form onSubmit={handleSubmit}>
 
-        <div>
-          <label>First Name</label>
-          <Field
-            name='firstName'
-            component='input'
-            placeholder='Fist Name'
-            validate={value => value ? undefined : 'Required'}
-          />
-        </div>
+          <div>
+            <Field name="firstName" placeholder="Fist Name" validate={required}>
+              {({ input, meta, placeholder }) => (
+                <div>
+                  <label>First Name</label>
+                  <input {...input} placeholder={placeholder} />
+                  {meta.error && meta.touched && <span>{meta.error}</span>}
+                </div>
+              )}
+            </Field>
+          </div>
 
-        <div>
-          <label>Last Name</label>
-          <Field
-            name='lastName'
-            component='input'
-            placeholder='Last Name'
-          />
-        </div>
+          <div>
+            <Field name="lastName" placeholder="Last Name" validate={required}>
+              {({ input, meta, placeholder }) => (
+                <div>
+                  <label>Last Name</label>
+                  <input {...input} placeholder={placeholder} />
+                  {meta.error && meta.touched && <span>{meta.error}</span>}
+                </div>
+              )}
+            </Field>
+          </div>
 
-        <div>
-          <label>Email</label>
-          <Field
-            name='email'
-            component='input'
-            placeholder='email'
-          />
-        </div>
-        <button type='submit' disabled={submitting}>Submit</button>
-        <pre>{JSON.stringify(values, undefined, 2)}</pre>
-      </form>}
+          <div>
+            <Field name="email" placeholder="email" validate={required} />
+          </div>
+          <button type="submit" disabled={submitting}>
+            Submit
+          </button>
+          <pre>{JSON.stringify(values, undefined, 2)}</pre>
+        </form>
+      )}
     </Form>
-
   </div>
-)
+);
 
 export default ReactFinalForm;
