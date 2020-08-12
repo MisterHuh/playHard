1e9804600f23b441a26bf1a3fdbd0c564ad27b49
MisterHuh <jaehuh86@yahoo.com>
2020-08-12 16:46:26 -0700

    implementing react-datepicker to react-final=form

--- client/components/redux_form2.jsx
+++ client/components/redux_form2.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const showResults = async (values) => {
   await sleep(500); // simulate server latency
@@ -15,10 +18,25 @@ const Form2 = () => (
         <form onSubmit={handleSubmit}>
           <div>
             <label>Date</label>
-            <Field name="date" component="input" placeholder="Date" />
+            <Field
+              name="date"
+              component={DatePicker}
+              placeholder="Date"
+              onChange={(event) => {
+                console.log("changing date to", event.target.value);
+                change("date", event.target.value);
+              }}
+            />
+            {/* <DatePicker value={values} /> */}
+          </div>
+
+          <div>
+            <label>Category</label>
+            <Field name="category" component="input" placeholder="Category" />
           </div>
 
           <button type="submit">Submit</button>
+
           <pre>{JSON.stringify(values, undefined, 2)}</pre>
         </form>
       )}
