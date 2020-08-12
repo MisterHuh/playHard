aee8e600c41c6d337814aea2b05c7b9ad41c1707
MisterHuh <jaehuh86@yahoo.com>
2020-08-12 16:13:07 -0700

    react final form - basic template added

---
+++ client/components/redux_form2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form2 = () => (
+  <div>
+    <h5>Date</h5>
+    <h5>Category</h5>
+    <h5>SubCategory</h5>
+    <h5>Credit Card</h5>
+    <h5>Amount</h5>
+    <h5>Desc</h5>
+    <h5>Notes</h5>
+  </div>
+);
+
+export default Form2;
--- client/containers/redux_add.jsx
+++ client/containers/redux_add.jsx
@@ -2,18 +2,30 @@
 import React from 'react';
 import SummaryBox from '../components/redux_summaryBox';
 import SideBox from '../components/redux_sideBox';
+
+
+/* forms */
 import Form from '../components/redux_form';
+import Form2 from '../components/redux_form2';
 import ReactFinalForm from '../components/redux_reactFinalForm'
-// import ReduxForm from '../components/redux_reduxForm';
+import ReduxForm from '../components/redux_reduxForm';
 
 // class Add extends Component {}
 
 const Add = () => (
     <React.Fragment>
-        {/* <Form />,
+
+        {/* regular form with state */}
+        {/* <Form />, */}
+
+        <Form2 />
         <SummaryBox />,
-        <SideBox /> */}
-        <ReactFinalForm />
+        <SideBox />
+
+        {/* practice react-final-form */}
+        {/* <ReactFinalForm> */}
+
+        {/* practice redux-form */}
         {/* <ReduxForm /> */}
     </React.Fragment>
 )
